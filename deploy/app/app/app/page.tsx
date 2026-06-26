e30b0a8bf9986b9a96ccbe6b57de00c589822d35
Skip715 <lisaskipton78@gmail.com>
2026-06-26 10:22:54 +0100

    Create page.tsx

---
+++ deploy/app/app/app/page.tsx
@@ -0,0 +1,17 @@
+import Hero from '@/components/Hero'
+import Benefits from '@/components/Benefits'
+import HowItWorks from '@/components/HowItWorks'
+import Equipment from '@/components/Equipment'
+import CTA from '@/components/CTA'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Benefits />
+      <HowItWorks />
+      <Equipment />
+      <CTA />
+    </>
+  )
+}
